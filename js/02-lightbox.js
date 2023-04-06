8af38495af479d56d8c217ccf61944c3c99032f8
Vadym Shatnii <vadymshatniy@gmail.com>
2023-04-06 16:13:40 +0300

    Update lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryGrid = document.querySelector(".gallery");
+
+const pictureItems = galleryItems.map((galleryItem) => {
+  const listItem = document.createElement('li');
+  listItem.classList.add("gallery__item");
+  listItem.innerHTML = `<a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      alt="${galleryItem.description}"
+    />
+  </a>`;
+  return listItem;
+});
+
+galleryGrid.append(...pictureItems);
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+
